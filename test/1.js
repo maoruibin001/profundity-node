6207f4ecac890e5d81193f8850adeeb64d98ca46
maoruibin001 <www.maoruibin@163.com>
2017-06-16 16:29:49 +0800

    commit mock service and not reference exisit request

---
+++ test/1.js
@@ -0,0 +1,6 @@
+/**
+ * Created by lenovo on 2017/6/16.
+ */
+var url = require('url');
+
+console.log(url.parse('http://127.0.0.1/user/user.json'))
--- www/simply_server.js
+++ www/simply_server.js
@@ -6,38 +6,59 @@ const fs = require('fs');
 const path = require('path');
 const url = require('url');
 const express = require('express');
+const request = require('request');
+
+const SERVER = 'http://127.0.0.1:8804';
 
 var app = express();
 
 
 app.use(function(req, res, next) {
-    const mockFile = path.resolve(__dirname, '../mock/' + req.url);
-    if (fs.existsSync(mockFile)) {
-        const stat = fs.statSync(mockFile);
-        if (stat.isFile()) {
-            let readStream = fs.createReadStream(mockFile);
-            res.writeHead(202, {
-                'Content-Type': 'text/html;charset=UTF-8',
-                'Cache-Control': 'no-cache, no-store, must-revalidate',
-                'Pragma': 'no-cache',
-                'Expires': '0',
-                'Content-Length': fs.statSync(mockFile).size
-            });
-            readStream.pipe(res);
-        } else {
-            res.status(404);
-            res.json('404 MOCK SHOULD NOT BE DIRECTORY')
+
+    let reqUrl = SERVER + req.url.substring(req.url.indexOf('/los/'));
+    var urlObj = url.parse(reqUrl);
+    let apiReqOpts = {
+        hostname: urlObj.hostname,
+        port: urlObj.port,
+        path: urlObj.path,
+        method: 'POST',
+        headers: {
+            'host': urlObj.hostname
         }
+    };
+    let apiReq = http.request(apiReqOpts, function(response) {
+        if (response.statusCode !== 200) {
+            const mockFile = path.resolve(__dirname, '../mock/' + req.url.substring(req.url.indexOf('/los/') + 5) + '.json');
+            if (fs.existsSync(mockFile)) {
+                const stat = fs.statSync(mockFile);
+                if (stat.isFile()) {
+                    let readStream = fs.createReadStream(mockFile);
+                    res.writeHead(202, {
+                        'Content-Type': 'text/html;charset=UTF-8',
+                        'Cache-Control': 'no-cache, no-store, must-revalidate',
+                        'Pragma': 'no-cache',
+                        'Expires': '0',
+                        'Content-Length': fs.statSync(mockFile).size
+                    });
+                    readStream.pipe(res);
+                } else {
+                    res.status(404);
+                    res.json('404 MOCK SHOULD NOT BE DIRECTORY')
+                }
 
 
-    } else {
-        res.status(404);
-        try {
-            return res.json('NOT FOUND');
-        } catch (e) {
-            console.log('404 SET HEADER AFTER SENT');
+            } else {
+                res.status(404);
+                try {
+                    return res.json('NOT FOUND');
+                } catch (e) {
+                    console.log('404 SET HEADER AFTER SENT');
+                }
+            }
         }
-    }
+
+    });
+    req.pipe(apiReq);
 });
 
 app.use(function(err, req, res, next) {
